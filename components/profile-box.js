cc2f2cadaacb872f40e513dae0e78af5b0cc985c
Bekir <l3ekirerdem@gmail.com>
2021-05-06 06:26:12 +0300

    profile-box componenti oluşturuldu

---
+++ components/profile-box.js
@@ -0,0 +1,24 @@
+import React from "react";
+import cn from "classnames";
+
+import styles from "./profile-box.module.css";
+
+import Photo from "./photo";
+import { Details } from "./icons";
+import Button from "./button";
+import TextBody from "./text-body";
+
+function ProfileBox({ name = "Bekir Erdem", slug = "l3ekirerdem" }) {
+  return (
+    <Button className={cn([styles.box])}>
+      <Photo />
+      <div className={styles.body}>
+        <TextBody bold>{name}</TextBody>
+        <TextBody className={styles.slug}>@{slug}</TextBody>
+      </div>
+      <Details className={styles.icon} />
+    </Button>
+  );
+}
+
+export default ProfileBox;
