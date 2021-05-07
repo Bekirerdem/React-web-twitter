b86335dac11e149daa212e5e0a926bb8a8b48851
Bekir <l3ekirerdem@gmail.com>
2021-05-07 16:52:34 +0300

    tarayıcı genişliğine göre durumları kontrol etmek için hooks eklendi

--- components/navigation.js
+++ components/navigation.js
@@ -16,52 +16,51 @@ import {
 
 import styles from "./navigation.module.css";
 
-function Navigation({ selectedKey }) {
+function Navigation({ flat = false, selectedKey }) {
   return (
     <nav className={styles.nav}>
-
       <NavButton>
-          <Twitter/>
+        <Twitter />
       </NavButton>
 
-      <NavButton selected={selectedKey === 'home'}>
-          <Home/>
-          <TextTitle>Home</TextTitle>
+      <NavButton selected={selectedKey === "home"}>
+        <Home />
+        <TextTitle>Home</TextTitle>
       </NavButton>
 
-      <NavButton selected={selectedKey === 'explore'}>
-          <Explore/>
-          <TextTitle>Explore</TextTitle>
+      <NavButton selected={selectedKey === "explore"}>
+        <Explore />
+        <TextTitle>Explore</TextTitle>
       </NavButton>
 
-      <NavButton notify={15} selected={selectedKey === 'notification'}>
-          <Notification/>
-          <TextTitle>Notification</TextTitle>
+      <NavButton notify={15} selected={selectedKey === "notification"}>
+        <Notification />
+        <TextTitle>Notification</TextTitle>
       </NavButton>
 
-      <NavButton selected={selectedKey === 'messages'}>
-          <Messages/>
-          <TextTitle>Messages</TextTitle>
+      <NavButton selected={selectedKey === "messages"}>
+        <Messages />
+        <TextTitle>Messages</TextTitle>
       </NavButton>
 
-      <NavButton selected={selectedKey === 'bookmarks'}>
-          <Bookmarks/>
-          <TextTitle>Bookmarks</TextTitle>
+      <NavButton selected={selectedKey === "bookmarks"}>
+        <Bookmarks />
+        <TextTitle>Bookmarks</TextTitle>
       </NavButton>
 
-      <NavButton selected={selectedKey === 'lists'}>
-          <Lists/>
-          <TextTitle>Lists</TextTitle>
+      <NavButton selected={selectedKey === "lists"}>
+        <Lists />
+        <TextTitle>Lists</TextTitle>
       </NavButton>
 
-      <NavButton selected={selectedKey === 'profile'}>
-          <Profile/>
-          <TextTitle>Profile</TextTitle>
+      <NavButton selected={selectedKey === "profile"}>
+        <Profile />
+        <TextTitle>Profile</TextTitle>
       </NavButton>
 
-      <NavButton selected={selectedKey === 'more'}>
-          <More/>
-          <TextTitle>More</TextTitle>
+      <NavButton selected={selectedKey === "more"}>
+        <More />
+        <TextTitle>More</TextTitle>
       </NavButton>
     </nav>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,21 @@
 import React from "react";
 
-import Button from "../components/button";
+import CONST from "../constants";
+import Layout from "../components/layout";
+import useWindowSize from "../hooks/useWindowSize";
+import SideBar from "../components/col-sidebar";
+import TimeLine from "../components/col-timeline";
+import Extra from "../components/col-extra";
 
 function HomePage() {
+  const size = useWindowSize()
+
   return (
-    <div>
-      <h1>Welcome to Next.js!</h1>
-      <Button>Merhaba</Button>
-    </div>
+    <Layout>
+      <SideBar flat={size.width < CONST.DESKTOP_SIZE}>Sidebar</SideBar>
+      <TimeLine>{JSON.stringify(size)}</TimeLine>
+      {size.width > CONST.TABLET_SIZE && <Extra>Extra</Extra>}
+    </Layout>
   );
 }
 
