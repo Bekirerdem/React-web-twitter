f3c798f8fdffb0467d0ad50d1b3e10ab4d84dbf4
Bekir <l3ekirerdem@gmail.com>
2021-05-08 05:25:03 +0300

    navigation refactor edildi

--- components/navigation.js
+++ components/navigation.js
@@ -1,67 +1,34 @@
 import React from "react";
+import { useRouter } from 'next/router'
+
+import { MENU } from "../constants";
+import styles from "./navigation.module.css";
 
 import NavButton from "./navigation-button";
 import TextTitle from "./text-title";
-import {
-  Twitter,
-  Home,
-  Explore,
-  Notification,
-  Messages,
-  Bookmarks,
-  Lists,
-  Profile,
-  More,
-} from "./icons";
 
-import styles from "./navigation.module.css";
+function Navigation({ flat = false}) {
+  const router = useRouter()
 
-function Navigation({ flat = false, selectedKey }) {
   return (
     <nav className={styles.nav}>
-      <NavButton>
-        <Twitter />
-      </NavButton>
-
-      <NavButton selected={selectedKey === "home"}>
-        <Home />
-        <TextTitle>Home</TextTitle>
-      </NavButton>
-
-      <NavButton selected={selectedKey === "explore"}>
-        <Explore />
-        <TextTitle>Explore</TextTitle>
-      </NavButton>
-
-      <NavButton notify={15} selected={selectedKey === "notification"}>
-        <Notification />
-        <TextTitle>Notification</TextTitle>
-      </NavButton>
-
-      <NavButton selected={selectedKey === "messages"}>
-        <Messages />
-        <TextTitle>Messages</TextTitle>
-      </NavButton>
-
-      <NavButton selected={selectedKey === "bookmarks"}>
-        <Bookmarks />
-        <TextTitle>Bookmarks</TextTitle>
-      </NavButton>
-
-      <NavButton selected={selectedKey === "lists"}>
-        <Lists />
-        <TextTitle>Lists</TextTitle>
-      </NavButton>
-
-      <NavButton selected={selectedKey === "profile"}>
-        <Profile />
-        <TextTitle>Profile</TextTitle>
-      </NavButton>
-
-      <NavButton selected={selectedKey === "more"}>
-        <More />
-        <TextTitle>More</TextTitle>
-      </NavButton>
+      {MENU.map((menu) => {
+        const showTitle = !flat && menu.title.length > 0;
+        const selected = router.pathname === menu.path;
+        
+        return (
+          <NavButton
+            key={menu.key}
+            notify={menu.notify}
+            selected={selected}
+            href={menu.path}
+            className={styles.navButton}
+          >
+            {selected ? menu.iconSelected : menu.icon}
+            {showTitle && <TextTitle>{menu.title}</TextTitle>}
+          </NavButton>
+        );
+      })}
     </nav>
   );
 }
