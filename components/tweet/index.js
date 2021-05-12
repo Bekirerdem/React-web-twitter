186730ffd057a3142ac23a4e28e75251738ea12a
Bekir <l3ekirerdem@gmail.com>
2021-05-13 02:56:18 +0300

    tweet componenti eklendi

--- components/tweet/index.js
+++ components/tweet/index.js
@@ -1,11 +1,12 @@
 import React from "react";
-// import formatDistanceToNowStrict from "date-fns/formatDistanceToNowStrict";
 
 import styles from "./style.module.css";
 
 import Photo from "../photo";
+import IconButton from "../icon";
+import * as Icon from "../icons";
 
-function Tweet({ name, slug, children }) {
+function Tweet({ name, slug, text }) {
   return (
     <article className={styles.tweet}>
       {/* Avatar */}
@@ -15,12 +16,45 @@ function Tweet({ name, slug, children }) {
       {/* Body */}
       <div className={styles.body}>
         <header className={styles.header}>
-          {name} @{slug} · 15 Hours
+          <span className={styles.name}>{name}</span> <span>@{slug}</span> ·{""}{" "}
+          <span>15 Hours</span>
         </header>
 
-        <div className={styles.content}>{children}</div>
+        <div className={styles.content}>{text}</div>
 
-        <footer className={styles.footer}>Footer</footer>
+        <footer className={styles.footer}>
+
+          {/* Reply */}
+          <div className={styles.footerButton}>
+            <IconButton className={styles.actionButton}>
+              <Icon.Reply />
+            </IconButton>
+            <span>2</span>
+          </div>
+
+          {/* Retweet */}
+          <div className={styles.footerButton}>
+            <IconButton className={styles.actionButton}>
+              <Icon.Retweet />
+            </IconButton>
+            <span>12</span>
+          </div>
+
+          {/* Like */}
+          <div className={styles.footerButton}>
+            <IconButton className={styles.actionButton}>
+              <Icon.Like />
+            </IconButton>
+            <span>85</span>
+          </div>
+
+          {/* Share */}
+          <div className={styles.footerButton}>
+            <IconButton className={styles.actionButton}>
+              <Icon.Share />
+            </IconButton>
+          </div>
+        </footer>
       </div>
     </article>
   );
