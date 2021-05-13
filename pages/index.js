cb24ef28fc405ba12dffd5f4f159d9912ba17230
Bekir <l3ekirerdem@gmail.com>
2021-05-13 03:00:33 +0300

    tweet componenti indexe uyarlandı

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,23 @@
 import React from "react";
 
 import Layout from "../components/layout";
-
+import Tweet from "../components/tweet/index";
 
 function HomePage() {
-
   return (
-    <Layout>Ana Sayfa</Layout>
+    <Layout>
+      <Tweet
+        name="Bekir Erdem"
+        slug="l3ekirerdem"
+        text={`belki bir gün dans ederiz marsta`}
+      />
+      <Tweet
+        name="Bekir Erdem"
+        slug="l3ekirerdem"
+        text={`En son yaprak kalana kadar ölmezmiş ağaç on üç yapraktan ilki düştü yere, 
+ayağa kalkmak için.`}
+      />
+    </Layout>
   );
 }
 
